acaf07f322c1e4cc3394b44e8cf803b5bb061218
DiyaYada <88617500+DiyaYada@users.noreply.github.com>
2022-04-14 12:47:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var Box1;
+
+function setup() 
+{
+  createCanvas(400, 400);
+
+Box1 = new Box(50,50,100,200,2);
+
+}
+
+
+function draw() 
+{
+  background(220);
+
+  Box1.show()
+Box1.move()
+
+}
+
